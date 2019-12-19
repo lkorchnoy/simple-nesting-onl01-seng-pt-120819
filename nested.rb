3b7c5a524dfa30e6418c8ad8cd971ab33b819020
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-19 17:46:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-programmer_hash[:adding_matz][:known_for][:languages] = 
+programmer_hash[:adding_matz][:known_for][:languages] = "COBOL", "FORTRAN"
 {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}
 end
 
